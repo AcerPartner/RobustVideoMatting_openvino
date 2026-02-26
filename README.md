ee6c696ba2096df4ff3ecc8509dfb5629ba348ce
unknown <joelan.hsieh@acer.com>
2026-02-26 02:56:46 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Robust Video Matting OpenVino version
+
+Robust Video Matting model is a powerful model that can provide high-quality matting images.
+
+For details see the [repository](https://github.com/PeterL1n/RobustVideoMatting). For model convert tool see [openvinotoolkit
+open_model_zoo](https://github.com/openvinotoolkit/open_model_zoo/tree/master/models/public/robust-video-matting-mobilenetv3)
+
+## How to convert Robust Video Matting to openvino
+1. Download robust-video-matting-mobilenetv3 from openvino model zoo
+```
+pip install openvino-dev
+omz_downloader --name robust-video-matting-mobilenetv3
+omz_converter --name robust-video-matting-mobilenetv3 --precisions FP16
+```
+2. Reshap model
+```
+pip install openvino==2025.3
+python .\reshape_ov.py
+```
+
+## Legal Information
+
+The original model is distributed under the
+[GPL-3.0 License](https://github.com/DmitriySidnev/RobustVideoMatting/blob/master/LICENSE).
---
+++ reshape_ov.py
@@ -0,0 +1,27 @@
+#Realse under GPL-3.0 license
+#Date 2026/02/26
+from openvino.runtime.passes import Manager, MakeStateful
+import openvino as ov
+
+#download model from runing 
+#  omz_downloader --name robust-video-matting-mobilenetv3
+#  omz_converter --name robust-video-matting-mobilenetv3 --precisions FP16
+#https://github.com/openvinotoolkit/open_model_zoo/tree/master/models/public/robust-video-matting-mobilenetv3
+
+HEIGHT = 360
+WIDTH = 640
+core = ov.Core()
+
+ov_input = {
+    'src': [1, 3, HEIGHT, WIDTH],
+    'r1': [1, 16, HEIGHT // 5, WIDTH // 5],
+    'r2': [1, 20, HEIGHT // 10, WIDTH // 10],
+    'r3': [1, 40, HEIGHT // 20, WIDTH // 20],
+    'r4': [1, 64, HEIGHT // 40, WIDTH // 40],
+}
+
+ov_model = core.read_model(model="./public/robust-video-matting-mobilenetv3/FP16/robust-video-matting-mobilenetv3.xml")
+
+ov_model.reshape(ov_input)
+ov.save_model(ov_model, f"./public/robust-video-matting-mobilenetv3/FP16/robust-video-matting-mobilenetv3_{WIDTH}x{HEIGHT}.xml")
+print("model done")
